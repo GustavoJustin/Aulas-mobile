03dca850c96583918c0f0ab2536ce6f334a2b483
GustavoJustin <gustavo.j.francisco@aluno.senai.br>
2026-03-09 16:20:34 -0300

    aula_flex

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import Exemplo01 from './jsx_examples/exemplo-01-estrutura';
 import Exemplo02 from './jsx_examples/exemplo-02-expressoes';
 import Exemplo03 from './jsx_examples/exemplo-03-condicionais';
 import Exemplo04 from './jsx_examples/exemplo-04-listas';
+
+//------------------------------------------------------------------------------------
+
 import Lista01 from './jsx_listas/Lista01';
 import Lista02 from './jsx_listas/Lista02';
 import Lista03 from './jsx_listas/Lista03';
 
+//-------------------------------------------------------------------------------------
+
+import ViewExamples1 from './jsx_flexbox_examples/flex_direction';
+import ViewExamples2 from './jsx_flexbox_examples/justify_content';
+import ViewExamples3 from './jsx_flexbox_examples/flex';
+import ViewExamples4 from './jsx_flexbox_examples/flex_wrap';
+
+//-------------------------------------------------------------------------------------
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Lista03 />
+      <ViewExamples4 />
     </View>
   );
 }
